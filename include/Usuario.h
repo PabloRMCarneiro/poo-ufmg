3f0af3ec00d8648e2db229775657b6b26f947457
Aquila Oliveira Souza <2021019327@cce.ufmg.br>
2022-10-04 15:27:07 -0300

    usuario ok

---
+++ include/Usuario.h
@@ -0,0 +1,24 @@
+#ifndef USUARIO_H_INCLUDED
+#define USUARIO_H_INCLUDED
+#include <string>
+#include <vector>
+using namespace std;
+class Usuario{
+    private:
+       
+        string email;
+        string senha;
+        vector <string> permissoes;
+     
+    public:
+        Usuario();
+        Usuario(string email, string senha);
+        ~Usuario();
+        string getEmail();
+        string getSenha();
+        void setEmail(string email);
+        void setSenha(string senha);
+        void setPermissoes(vector <string> permissoes);
+};
+
+#endif 
---
+++ src/Usuario.cpp
@@ -0,0 +1,28 @@
+#include "../include/Usuario.h";
+
+Usuario::Usuario(){
+    email = "";
+    senha = "";
+}
+Usuario::Usuario(string email, string senha){
+    this->email = email;
+    this->senha = senha;
+}
+Usuario::~Usuario(){
+}
+string Usuario::getEmail(){
+    return email;
+}
+string Usuario::getSenha(){
+    return senha;
+}
+void Usuario::setEmail(string email){
+    this->email = email;
+}
+void Usuario::setSenha(string senha){
+    this->senha = senha;
+}
+void Usuario::setPermissoes(vector <string> permissoes){
+    this->permissoes = permissoes;
+}
+
