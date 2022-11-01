06e26fd5a5a483df0ca194d34248b9b9a6fbab1f
Pablo Carneiro <2020021662@cce.ufmg.br>
2022-11-01 16:09:29 -0300

    feat: interface Sprint 6

---
+++ include/OrdemDeCompra.h
@@ -0,0 +1,34 @@
+#ifndef ORDEMDECOMPRA_H_INCLUDED
+#define ORDEMDECOMPRA_H_INCLUDED
+#include "./include/Orcamento.h"
+#include "./include/Fornecedor.h"
+#include "./include/MateriaPrima.h"
+#include "./include/data.h"
+#include <string>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+class OrdemDeCompra{
+    private:
+        vector<MateriaPrima*> materiaPrima;
+        Data dataOrdem;
+        double valorTotal;
+        vector <pair<Fornecedor*, MateriaPrima*>> melhoresPrecos;
+        vector <Orcamento*> orcamento;
+        bool compra;
+    public:
+        OrdemDeCompra();
+        void gerarPedidos(MateriaPrima*, double);
+        void setData(Data valData);
+        void setMelhoresPrecos(vector <pair<Fornecedor*, MateriaPrima*>> valMelhoresPrecos);
+        void setValorTotal(double valValorTotal);
+        void setCompra(bool valCompra);
+
+        Data getDatOrdem();
+        double getValorTotal();
+        vector <Orcamento*> getOrcamento();
+};
+
+#endif
