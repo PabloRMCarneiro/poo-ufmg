22ffdfa0e3605c6e98324276f31995a4ab58a8f6
Aquila Oliveira Souza <2021019327@cce.ufmg.br>
2022-11-01 15:49:15 -0300

    feat:CartaoDeCredito.cpp

---
+++ src/CartaoDeCredito.cpp
@@ -0,0 +1,48 @@
+#include "./MetodoDePagamento.h"
+#include "./CartaoDeCredito.h"
+
+using namespace std;
+
+CartaoDeCredito::CartaoDeCredito() : MetodoDePagamento() {
+  this->numeroDoCartao = "";
+  this->nomeDoTitular = "";
+  this->codigoDeSeguranca = "";
+  this->dataDeValidade = Data();
+}
+
+CartaoDeCredito::CartaoDeCredito(string numeroDoCartao, string nomeDoTitular,
+                                 string codigoDeSeguranca, Data dataDeValidade)
+    : MetodoDePagamento() {
+  this->numeroDoCartao = numeroDoCartao;
+  this->nomeDoTitular = nomeDoTitular;
+  this->codigoDeSeguranca = codigoDeSeguranca;
+  this->dataDeValidade = dataDeValidade;
+}
+
+CartaoDeCredito::~CartaoDeCredito() {}
+
+string CartaoDeCredito::getNumeroDoCartao() { return this->numeroDoCartao; }
+
+string CartaoDeCredito::getNomeDoTitular() { return this->nomeDoTitular; }
+
+string CartaoDeCredito::getCodigoDeSeguranca() {
+  return this->codigoDeSeguranca;
+}
+
+Data CartaoDeCredito::getDataDeValidade() { return this->dataDeValidade; }
+
+void CartaoDeCredito::setNumeroDoCartao(string numeroDoCartao) {
+  this->numeroDoCartao = numeroDoCartao;
+}
+
+void CartaoDeCredito::setNomeDoTitular(string nomeDoTitular) {
+  this->nomeDoTitular = nomeDoTitular;
+}
+
+void CartaoDeCredito::setCodigoDeSeguranca(string codigoDeSeguranca) {
+  this->codigoDeSeguranca = codigoDeSeguranca;
+}
+
+void CartaoDeCredito::setDataDeValidade(Data dataDeValidade) {
+  this->dataDeValidade = dataDeValidade;
+}
