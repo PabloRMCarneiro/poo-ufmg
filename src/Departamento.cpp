a5d995781e75be68344035352211b669ba4a5c30
PabloRMCarneiro <pablormc3dd3@gmail.com>
2022-11-27 11:30:34 -0300

    fix: correct throw and permission denied in Departamento.cpp

--- src/Departamento.cpp
+++ src/Departamento.cpp
@@ -1,15 +1,18 @@
 #include "../include/Departamento.h"
 #include "../include/Empresa.h"
 #include "../include/UsuarioLogado.h"
+#include "../include/PermissaoNegada.h"
 #include <string>
 
 using namespace std;
 
 Departamento::Departamento()
 {
-  if (false)//!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Departamento.Departamento"))
+  if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Departamento.Departamento"))
   {
-    throw "Acesso negado a Departamento.Departamento";
+    PermissaoNegada *a = new PermissaoNegada("Departamento", "Departamento");
+    string mensagem = "Acesso negado a Departamento.Departamento";
+    throw mensagem;
   }
   else
   {
@@ -19,10 +22,11 @@ Departamento::Departamento()
 
 Departamento::Departamento(string valNome)
 {
-  if (false)//!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Departamento.Departamento"))
+  if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Departamento.Departamento"))
   {
     PermissaoNegada *a = new PermissaoNegada("Departamento", "Departamento");
-    throw "Acesso negado a Departamento.Departamento";
+    string mensagem = "Acesso negado a Departamento.Departamento";
+    throw mensagem;
   }
   else
   {
@@ -36,10 +40,11 @@ Departamento::~Departamento()
 
 string Departamento::getNome()
 {
-  if (false)//!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Departamento.getNome"))
+  if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Departamento.getNome"))
   {
     PermissaoNegada *a = new PermissaoNegada("getNome", "Departamento");
-    throw "Acesso negado a Departamento.getNome";
+    string mensagem = "Acesso negado a Departamento.getNome";
+    throw mensagem;
   }
   else
   { 
@@ -50,10 +55,11 @@ string Departamento::getNome()
 
 void Departamento::setNome(string valNome)
 { 
-  if (false)//!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Departamento.getNome"))
+  if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Departamento.getNome"))
   {
     PermissaoNegada *a = new PermissaoNegada("setNome", "Departamento");
-    throw "Acesso negado a Departamento.setNome";
+    string mensagem = "Acesso negado a Departamento.setNome";
+    throw mensagem;
   }
   else
   {
