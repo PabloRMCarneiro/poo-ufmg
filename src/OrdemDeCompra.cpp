90d84e6ce0f239ec53693bcd698f071d08906417
PabloRMCarneiro <pablormc3dd3@gmail.com>
2022-11-27 13:24:16 -0300

    fix: correct throw and permission denied in OrdemDeCompra.cpp

--- src/OrdemDeCompra.cpp
+++ src/OrdemDeCompra.cpp
@@ -1,11 +1,14 @@
 #include "../include/OrdemDeCompra.h"
 #include "../include/UsuarioLogado.h"
+#include "../include/PermissaoNegada.h"
 
 OrdemDeCompra::OrdemDeCompra()
 {
-    if (false)//!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.OrdemDeCompra"))
+    if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.OrdemDeCompra"))
     {
-        throw "Acesso negado a OrdemDeCompra.OrdemDeCompra";
+        PermissaoNegada *a = new PermissaoNegada("OrdemDeCompra", "OrdemDeCompra");
+        string mensagem = "Acesso negado a OrdemDeCompra.OrdemDeCompra";
+        throw mensagem;
     }
     else
     {
@@ -16,9 +19,11 @@ OrdemDeCompra::OrdemDeCompra()
 
 OrdemDeCompra::OrdemDeCompra(MateriaPrima *valMateriaPrima)
 {
-    if (false)//!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.OrdemDeCompra"))
+    if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.OrdemDeCompra"))
     {
-        throw "Acesso negado a OrdemDeCompra.OrdemDeCompra";
+        PermissaoNegada *a = new PermissaoNegada("OrdemDeCompra", "OrdemDeCompra");
+        string mensagem = "Acesso negado a OrdemDeCompra.OrdemDeCompra";
+        throw mensagem;
     }
     else
     {
@@ -30,44 +35,93 @@ OrdemDeCompra::OrdemDeCompra(MateriaPrima *valMateriaPrima)
 
 void OrdemDeCompra::setMateriaPrimaLista(vector<MateriaPrima *> valMateriaPrima)
 {
-    for (auto it : valMateriaPrima)
+    if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.setMateriaPrimaLista"))
     {
-        this->setMateriaPrima(it);
+        PermissaoNegada *a = new PermissaoNegada("setMateriaPrimaLista", "OrdemDeCompra");
+        string mensagem = "Acesso negado a OrdemDeCompra.setMateriaPrimaLista";
+        throw mensagem;
+    }
+    else
+    {
+        for (auto it : valMateriaPrima)
+        {
+            this->setMateriaPrima(it);
+        }
     }
 }
 
 void OrdemDeCompra::setMateriaPrima(MateriaPrima *valMateriaPrima)
 {
-    this->materiaPrimaLista.push_back(valMateriaPrima);
+    if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.setMateriaPrima"))
+    {
+        PermissaoNegada *a = new PermissaoNegada("setMateriaPrima", "OrdemDeCompra");
+        string mensagem = "Acesso negado a OrdemDeCompra.setMateriaPrima";
+        throw mensagem;
+    }
+    else
+    {
+        this->materiaPrimaLista.push_back(valMateriaPrima);
+    }
 }
 
 void OrdemDeCompra::setData(Data valData)
 {
-    this->dataOrdem = valData;
+    if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.setData"))
+    {
+        PermissaoNegada *a = new PermissaoNegada("setData", "OrdemDeCompra");
+        string mensagem = "Acesso negado a OrdemDeCompra.setData";
+        throw mensagem;
+    }
+    else
+    {
+        this->dataOrdem = valData;
+    }
 }
 
 void OrdemDeCompra::setValorTotal(double valValorTotal)
 {
-    this->valorTotal = this->valorTotal + valValorTotal;
+    if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.setValorTotal"))
+    {
+        PermissaoNegada *a = new PermissaoNegada("setValorTotal", "OrdemDeCompra");
+        string mensagem = "Acesso negado a OrdemDeCompra.setValorTotal";
+        throw mensagem;
+    }
+    else
+    {
+        this->valorTotal = this->valorTotal + valValorTotal;
+    }
 }
 
 void OrdemDeCompra::setOrcamento()
-{    
-    vector<pair<string, double>> valItens;
-    for(auto it : materiaPrimaLista){
-        valItens.push_back(make_pair(it->getNome(), it->getEstoqueMinimo()));
+{
+    if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.setOrcamento"))
+    {
+        PermissaoNegada *a = new PermissaoNegada("setOrcamento", "OrdemDeCompra");
+        string mensagem = "Acesso negado a OrdemDeCompra.setOrcamento";
+        throw mensagem;
     }
-    for(auto it : Fornecedor::fornecedorList){
-        Orcamento * novo = new Orcamento(false, valItens, Data::dataDeHoje, it);
-        this->orcamentos.push_back(novo);
+    else
+    {
+        vector<pair<string, double>> valItens;
+        for (auto it : materiaPrimaLista)
+        {
+            valItens.push_back(make_pair(it->getNome(), it->getEstoqueMinimo()));
+        }
+        for (auto it : Fornecedor::fornecedorList)
+        {
+            Orcamento *novo = new Orcamento(false, valItens, Data::dataDeHoje, it);
+            this->orcamentos.push_back(novo);
+        }
     }
 }
 
 void OrdemDeCompra::gerarPedidos()
 {
-    if (false)//!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.gerarPedidos"))
+    if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.gerarPedidos"))
     {
-        throw "Acesso negado a OrdemDeCompra.gerarPedidos";
+        PermissaoNegada *a = new PermissaoNegada("gerarPedidos", "OrdemDeCompra");
+        string mensagem = "Acesso negado a OrdemDeCompra.gerarPedidos";
+        throw mensagem;
     }
     else
     {
@@ -80,34 +134,52 @@ void OrdemDeCompra::gerarPedidos()
 
 void OrdemDeCompra::setMelhoresPrecos()
 {
-    double menorPreco = 0.0, precoAtual = 0.0;
-    Fornecedor *forneceMenorPreco;
-    for (auto it : materiaPrimaLista)
+    if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.setMelhoresPrecos"))
     {
-        for (auto it2 : this->orcamentos)
+        PermissaoNegada *a = new PermissaoNegada("setMelhoresPrecos", "OrdemDeCompra");
+        string mensagem = "Acesso negado a OrdemDeCompra.setMelhoresPrecos";
+        throw mensagem;
+    }
+    else
+    {
+        double menorPreco = 0.0, precoAtual = 0.0;
+        Fornecedor *forneceMenorPreco;
+        for (auto it : materiaPrimaLista)
         {
-            for (auto it3 : it2->getPrecos())
+            for (auto it2 : this->orcamentos)
             {
-                if (it3.first == it->getNome())
+                for (auto it3 : it2->getPrecos())
                 {
-                    if (menorPreco = 0.0)
+                    if (it3.first == it->getNome())
                     {
-                        menorPreco = it3.second;
-                    }
-                    else if (menorPreco > it3.second)
-                    {
-                        menorPreco = it3.second;
-                        forneceMenorPreco = it2->getFornecedor();
+                        if (menorPreco = 0.0)
+                        {
+                            menorPreco = it3.second;
+                        }
+                        else if (menorPreco > it3.second)
+                        {
+                            menorPreco = it3.second;
+                            forneceMenorPreco = it2->getFornecedor();
+                        }
                     }
                 }
             }
+            this->melhoresPrecos.push_back(make_pair(forneceMenorPreco, it));
+            this->setValorTotal(menorPreco);
         }
-        this->melhoresPrecos.push_back(make_pair(forneceMenorPreco, it));
-        this->setValorTotal(menorPreco);
     }
 }
 
 void OrdemDeCompra::setCompra(bool valCompra)
 {
-    this->compra = valCompra;
+    if (!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("OrdemDeCompra.setCompra"))
+    {
+        PermissaoNegada *a = new PermissaoNegada("setCompra", "OrdemDeCompra");
+        string mensagem = "Acesso negado a OrdemDeCompra.setCompra";
+        throw mensagem;
+    }
+    else
+    {
+        this->compra = valCompra;
+    }
 }
