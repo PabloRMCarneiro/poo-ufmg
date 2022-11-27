09f5dc79f06d0b6251bb6cb22d30c047bc80e30b
PabloRMCarneiro <pablormc3dd3@gmail.com>
2022-11-27 17:23:24 -0300

    fix: correct throw and permission denied in Empresa.cpp

--- src/Empresa.cpp
+++ src/Empresa.cpp
@@ -1,146 +1,367 @@
 #include "../include/Empresa.h"
 #include "../include/LogEscrita.h"
 #include "../include/LogLeitura.h"
+#include "../include/UsuarioLogado.h"
 
 using namespace std;
 
 Empresa *Empresa::empresa = nullptr;
 
-Empresa::Empresa(){
-
-}
+Empresa::Empresa(){}
 
 Empresa *Empresa::getEmpresa(){
-  if(empresa ==nullptr){
-    empresa = new Empresa;
+
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getEmpresa"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getEmpresa", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getEmpresa";
+    throw mensagem;
   }
-  return empresa;
+  else{
+    if(empresa ==nullptr){
+      empresa = new Empresa;
+    }
+    return empresa;
+  }
 }
 
 vector <LogEscrita*> Empresa::getlogsEscrita(){
-  return this->logsEscrita;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getlogsEscrita"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getlogsEscrita", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getlogsEscrita";
+    throw mensagem;
+  }
+  else{
+    return this->logsEscrita;
+  }
 }
 vector<RegistroVendas*> Empresa::getRegistrosVendas(){
-  return this->registrosVendas;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getRegistrosVendas"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getRegistrosVendas", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getRegistrosVendas";
+    throw mensagem;
+  }
+  else{
+    return this->registrosVendas;
+  }
 }
 vector <LogLeitura*> Empresa::getlogsLeitura(){
-  return this->logsLeitura;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getlogsLeitura"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getlogsLeitura", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getlogsLeitura";
+    throw mensagem;
+  }
+  else{
+    return this->logsLeitura;
+  }
 }
 void Empresa::setlogAcessoNegado(PermissaoNegada* valPermissaoNegada){
-  this->logsAcessoNegado.push_back(valPermissaoNegada);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setlogAcessoNegado"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setlogAcessoNegado", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setlogAcessoNegado";
+    throw mensagem;
+  }
+  else{
+    this->logsAcessoNegado.push_back(valPermissaoNegada);
+  }
 }
 vector<PermissaoNegada*> Empresa::getLogAcesso(){
-  return this->logsAcessoNegado;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getLogAcesso"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getLogAcesso", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getLogAcesso";
+    throw mensagem;
+  }
+  else{
+    return this->logsAcessoNegado;
+  }
 }
 MateriaPrima* Empresa::getMateriaPrima(string valMateriaPrima){
-  for(auto it : this->materiaPrimaLista){
-    if(it->getNome() == valMateriaPrima){
-      return it;
-    }
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getMateriaPrima"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getMateriaPrima", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getMateriaPrima";
+    throw mensagem;
   }
-  return nullptr;
+  else{
+    for(auto it : this->materiaPrimaLista){
+    if(it->getNome() == valMateriaPrima){
+        return it;
+      }
+    }
+    return nullptr;
+  }
+  
 }
 
 vector<MateriaPrima*> Empresa::getMateriaPrimaLista(){
-  return this->materiaPrimaLista;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getMateriaPrimaLista"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getMateriaPrimaLista", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getMateriaPrimaLista";
+    throw mensagem;
+  }
+  else{
+    return this->materiaPrimaLista;
+  }
 }
 
 void Empresa::setMateriaPrima(MateriaPrima* valMateriaPrima){
-  this->materiaPrimaLista.push_back(valMateriaPrima);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setMateriaPrima"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setMateriaPrima", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setMateriaPrima";
+    throw mensagem;
+  }
+  else{
+    this->materiaPrimaLista.push_back(valMateriaPrima);
+  }
 }
 
 void Empresa::setMateriaPrimaLista(vector<MateriaPrima*> valMateriaPrima){
-  this->materiaPrimaLista = valMateriaPrima;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setMateriaPrimaLista"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setMateriaPrimaLista", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setMateriaPrimaLista";
+    throw mensagem;
+  }
+  else{
+    this->materiaPrimaLista = valMateriaPrima;
+  }
 }
 
 void Empresa::setOrdemDeCompra(OrdemDeCompra* valOrdemDeCompra){
-  this->ordemDeCompraLista.push_back(valOrdemDeCompra);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setOrdemDeCompra"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setOrdemDeCompra", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setOrdemDeCompra";
+    throw mensagem;
+  }
+  else{
+    this->ordemDeCompraLista.push_back(valOrdemDeCompra);
+  }
 }
 
 void Empresa::setOrdemDeProducao(OrdemDeProducao* valOrdemDeProducao){
-  this->ordemDeProducaoLista.push_back(valOrdemDeProducao);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setOrdemDeProducao"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setOrdemDeProducao", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setOrdemDeProducao";
+    throw mensagem;
+  }
+  else{
+    this->ordemDeProducaoLista.push_back(valOrdemDeProducao);
+  }
 }
 
 vector<OrdemDeCompra*> Empresa::getOrdemDeCompraLista(){
-  return this->ordemDeCompraLista;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getOrdemDeCompraLista"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getOrdemDeCompraLista", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getOrdemDeCompraLista";
+    throw mensagem;
+  }
+  else{
+    return this->ordemDeCompraLista;
+  }
 }
 
 vector<OrdemDeProducao*> Empresa::getOrdemDeProducaoLista(){
-  return this->ordemDeProducaoLista;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getOrdemDeProducaoLista"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getOrdemDeProducaoLista", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getOrdemDeProducaoLista";
+    throw mensagem;
+  }
+  else{
+    return this->ordemDeProducaoLista;
+  }
 }
 
  Cargo * Empresa::getCargo(string valCargo){
-  for (int i = 0; i < empresa->cargos.size(); i++)
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getCargo"))
   {
-    if (empresa->cargos[i]->getNome() == valCargo)
-    {
-      return empresa->cargos[i];
-    }
+    PermissaoNegada *a = new PermissaoNegada("getCargo", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getCargo";
+    throw mensagem;
   }
-  return nullptr;
+  else{
+    for (int i = 0; i < empresa->cargos.size(); i++)
+    {
+    if (empresa->cargos[i]->getNome() == valCargo)
+      {
+      return empresa->cargos[i];
+      }
+    }
+    return nullptr;
+  }
+  
 }
 
 UsuarioLogado* Empresa::getUsuario(){
-  return this->usuarioLogado;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getUsuario"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getUsuario", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getUsuario";
+    throw mensagem;
+  }
+  else{
+    return this->usuarioLogado;
+  }
 }
 
 void Empresa::login(UsuarioLogado* valUsuario){
-  this->usuarioLogado = valUsuario;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.login"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("login", "Empresa");
+    string mensagem = "Acesso negado a Empresa.login";
+    throw mensagem;
+  }
+  else{
+    this->usuarioLogado = valUsuario;
+  }
 }
 
 void Empresa::setlogEscrita(LogEscrita* valLogEscrita){
-  this->logsEscrita.push_back(valLogEscrita);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setlogEscrita"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setlogEscrita", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setlogEscrita";
+    throw mensagem;
+  }
+  else{
+    this->logsEscrita.push_back(valLogEscrita);
+  }
 }
 
 void Empresa::setlogLeitura(LogLeitura* valLogLeitura){
-  this->logsLeitura.push_back(valLogLeitura);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setlogLeitura"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setlogLeitura", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setlogLeitura";
+    throw mensagem;
+  }
+  else{
+    this->logsLeitura.push_back(valLogLeitura);
+  }
 }
 
 void Empresa::setCargo(Cargo *valCargo)
 {
-  empresa->cargos.push_back(valCargo);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setCargo"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setCargo", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setCargo";
+    throw mensagem;
+  }
+  else{
+    this->cargos.push_back(valCargo);
+  }
 }
 
 void Empresa::setEndereco(Endereco valEndereco){
-  this->endereco = valEndereco;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setEndereco"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setEndereco", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setEndereco";
+    throw mensagem;
+  }
+  else{
+    this->endereco = valEndereco;
+  }
 }
 
 Endereco Empresa::getEndereco(){
-  return this->endereco;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getEndereco"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getEndereco", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getEndereco";
+    throw mensagem;
+  }
+  else{
+    return this->endereco;
+  }
 }
 
 Departamento *Empresa::getDepartamento(string name){
-  for(auto it : departamentos){
-    if(it->getNome() == name){
-      return it;
-    }
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getDepartamento"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getDepartamento", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getDepartamento";
+    throw mensagem;
   }
-  return nullptr;
+  else{ 
+    for(auto it : departamentos){
+      if(it->getNome() == name){
+        return it;
+      }
+    }
+    return nullptr;
+  }
 }
+
 Categoria* Empresa::getCategoria(string valCategoria){
-  for(auto it : categorias){
-    if(it->getTipo()==valCategoria){
-      return it;
-    }
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getCategoria"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getCategoria", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getCategoria";
+    throw mensagem;
   }
-  return nullptr;
+  else{
+    for(auto it : categorias){
+      if(it->getTipo()==valCategoria){
+        return it;
+      }
+    }
+    return nullptr;
+  }
 }
+
 void Empresa::setCategoria(Categoria* valCategoria){
-  if(getCategoria(valCategoria->getTipo())==nullptr){
-    this->categorias.push_back(valCategoria);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setCategoria"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setCategoria", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setCategoria";
+    throw mensagem;
   }
-}
-Turno* Empresa::getTurno(string valTurno){
-  for(auto it : this->turnos){
-    if(it->getPeriodo() == valTurno){
-      return it;
+  else{
+    if(getCategoria(valCategoria->getTipo())==nullptr){
+      this->categorias.push_back(valCategoria);
     }
   }
-  return nullptr;
+}
+
+Turno* Empresa::getTurno(string valTurno){
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getTurno"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getTurno", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getTurno";
+    throw mensagem;
+  }
+  else{
+    for(auto it : this->turnos){
+      if(it->getPeriodo() == valTurno){
+        return it;
+      }
+    }
+    return nullptr;
+  }
 }
 
 vector<Turno*> Empresa::getTurnos(){
-  return this->turnos;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getTurnos"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getTurnos", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getTurnos";
+    throw mensagem;
+  }
+  else{
+    return this->turnos;
+  }
 }
 void Empresa::excluiFuncionario(Funcionario *valFuncionario){
   if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.excluiFuncionario"))
@@ -161,79 +382,191 @@ void Empresa::excluiFuncionario(Funcionario *valFuncionario){
 }
 
 Veiculo* Empresa::getVeiculo(Turno* valTurno){
-  for(auto it : this->veiculos){
-    if(it->getTurno() == valTurno){
-      return it;
-    }
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getVeiculo"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getVeiculo", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getVeiculo";
+    throw mensagem;
   }
-  return nullptr;
+  else{
+    for(auto it : this->veiculos){
+      if(it->getTurno() == valTurno){
+        return it;
+      }
+    }
+    return nullptr;
+  }
 }
 
 vector<Veiculo*> Empresa::getVeiculos(){
-  return this->veiculos;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getVeiculos"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getVeiculos", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getVeiculos";
+    throw mensagem;
+  }
+  else{
+    return this->veiculos;
+  }
 }
 
 void Empresa::setTurno(Turno* valTurno){
-  this->turnos.push_back(valTurno);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setTurno"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setTurno", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setTurno";
+    throw mensagem;
+  }
+  else{
+    this->turnos.push_back(valTurno);
+  }
 }
 
 void Empresa::setTurnos(vector<Turno*> valTurnos){
-  this->turnos = valTurnos;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setTurnos"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setTurnos", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setTurnos";
+    throw mensagem;
+  }
+  else{
+    this->turnos = valTurnos;
+  }
 }
 
 void Empresa::setVeiculo(Veiculo* valVeiculo){
-  this->veiculos.push_back(valVeiculo);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setVeiculo"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setVeiculo", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setVeiculo";
+    throw mensagem;
+  }
+  else{
+    this->veiculos.push_back(valVeiculo);
+  }
 }
 
 void Empresa::setVeiculos(vector<Veiculo*> valVeiculos){
-  this->veiculos = valVeiculos;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setVeiculos"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setVeiculos", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setVeiculos";
+    throw mensagem;
+  }
+  else{
+    this->veiculos = valVeiculos;
+  }
 }
 
 void Empresa::setDepartamento(Departamento *valDepartamento){
-  empresa->departamentos.push_back(valDepartamento);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setDepartamento"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setDepartamento", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setDepartamento";
+    throw mensagem;
+  }
+  else{
+    this->departamentos.push_back(valDepartamento);
+  }
 }
 
 Cliente *Empresa::getCliente(string doc){
-
-  for(auto it : clientes){
-    if(it->getDocumento() == doc){
-      return it;
-    }
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getCliente"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getCliente", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getCliente";
+    throw mensagem;
   }
-  return nullptr;
+  else{
+    for(auto it : clientes){
+      if(it->getDocumento() == doc){
+        return it;
+      }
+    }
+    return nullptr;
+  }
 }
 
 void Empresa::setCliente(Cliente *valCliente){
-  empresa->clientes.push_back(valCliente);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setCliente"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setCliente", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setCliente";
+    throw mensagem;
+  }
+  else{
+    empresa->clientes.push_back(valCliente);
+  }
 }
 
 Funcionario *Empresa::getFuncionario(string doc){
-  for(auto it : funcionarios){
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getFuncionario"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getFuncionario", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getFuncionario";
+    throw mensagem;
+  }
+  else{
+    for(auto it : funcionarios){
     if( it->getDocumento() == doc){
       return it;
+      }
     }
+    return nullptr;
   }
-  return nullptr;
+  
 }
 
 vector<Funcionario*> Empresa::getFuncionarios(){
-  return this->funcionarios;
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getFuncionarios"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getFuncionarios", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getFuncionarios";
+    throw mensagem;
+  }
+  else{
+    return this->funcionarios;
+  }
 }
 
 void Empresa::setFuncionario(Funcionario *valFuncionario){
-  empresa->funcionarios.push_back(valFuncionario);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setFuncionario"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setFuncionario", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setFuncionario";
+    throw mensagem;
+  }
+  else{
+    empresa->funcionarios.push_back(valFuncionario);
+  }
 }
 
 RegistroVendas *Empresa::getRegistroVendas(Data valData, Produto* valProduto, Cliente* valCliente){
-  for(auto it : registrosVendas){
-    if((it->getDataDeVenda() == valData) && (it->getProduto() == valProduto) && (it->getCliente()== valCliente)){
-      return it;
-    }
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.getRegistroVendas"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("getRegistroVendas", "Empresa");
+    string mensagem = "Acesso negado a Empresa.getRegistroVendas";
+    throw mensagem;
   }
-  return nullptr;
+  else{
+    for(auto it : registrosVendas){
+      if((it->getDataDeVenda() == valData) && (it->getProduto() == valProduto) && (it->getCliente()== valCliente)){
+        return it;
+      }
+    }
+    return nullptr;
+  }
 }
 
 
 void Empresa::setRegistroVendas(RegistroVendas *valRegistro){
-  empresa->registrosVendas.push_back(valRegistro);
+  if(!UsuarioLogado::getUsuarioLogado()->getUsuario()->getPermissoes("Empresa.setRegistroVendas"))
+  {
+    PermissaoNegada *a = new PermissaoNegada("setRegistroVendas", "Empresa");
+    string mensagem = "Acesso negado a Empresa.setRegistroVendas";
+    throw mensagem;
+  }
+  else{
+    empresa->registrosVendas.push_back(valRegistro);
+  }
 }
